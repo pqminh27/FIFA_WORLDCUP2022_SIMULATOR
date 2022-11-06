bb5c601a2acfabf32031c6dea6358c9ab7d272c1
pqminh27 <76590223+pqminh27@users.noreply.github.com>
2022-11-07 00:13:30 +0300

    Update ResizeImg.py

--- ResizeImg.py
+++ ResizeImg.py
@@ -1,4 +1,4 @@
 from PIL import Image
 image = Image.open("images/FIFA-World-Cup-Winner.gif")
-image = image.resize((500, 400),Image.ANTIALIAS)
+image = image.resize((500, 300),Image.ANTIALIAS)
 image.save(fp="images/FIFA-World-Cup-Winner.gif")
